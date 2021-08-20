5a5376a1558959db399f0c516d8cf8ad9fecc888
Or Asher <orasher556@gmail.com>
2021-08-20 12:08:15 +0300

    Added continual data generation to generator

--- data_generator.py
+++ data_generator.py
@@ -1,3 +1,4 @@
+import time
 from socket import socket, AF_INET, SOCK_STREAM
 
 from numpy.random import default_rng
@@ -5,6 +6,9 @@ from numpy.random import default_rng
 
 class DataGenerator:
     _VECTOR_LENGTH = 50
+    _FREQUENCY = 1
+    _PERIOD = 1.0 / _FREQUENCY
+    _PRECISION = 0.001
 
     def __init__(self, host, port):
         self.host = host
@@ -12,10 +16,14 @@ class DataGenerator:
         self.rng = default_rng()
 
     def start(self):
-        with socket(AF_INET, SOCK_STREAM) as sock:
-            sock.connect((self.host, self.port))
-            sock.sendall(self.generate_vector())
+        while True:
+            with socket(AF_INET, SOCK_STREAM) as sock:
+                sock.connect((self.host, self.port))
+                time_before = time.time()
+                print("Hello")
+                sock.sendall(self.generate_vector())
+            while (time.time() - time_before) < self._PERIOD:
+                time.sleep(self._PRECISION)
 
     def generate_vector(self):
         return self.rng.normal(size=self._VECTOR_LENGTH).tobytes()
-
