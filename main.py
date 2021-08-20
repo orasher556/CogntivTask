a518cc843b7c4b08f33090b94d824434712bc882
Or Asher <orasher556@gmail.com>
2021-08-20 11:50:42 +0300

    Added basic run logic to main

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import socket
+from threading import Thread
+
+from data_analyzer_server import DataAnalyzerServer
+from data_generator import DataGenerator
+
 if __name__ == '__main__':
-    
+    HOST, PORT = "localhost", 9999
+
+    with DataAnalyzerServer(HOST, PORT) as server:
+        analyzer_thread = Thread(target=server.serve_forever)
+        analyzer_thread.daemon = True
+        analyzer_thread.start()
+
+        generator = DataGenerator(HOST, PORT)
+        generator.start()
